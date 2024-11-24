95488c845506895fa7fee8220e8febfc50b0b05c
ColbyDomingos <colbydomingos@my.whccd.edu>
2024-11-23 18:11:22 -0800

    added the foreground colors with comments

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
 window.onload = function () { //Initializes when the window of the website loads
     const savedName = localStorage.getItem("name"); //gets the saved name from what I set it as
     const savedColor = localStorage.getItem("background-color"); //gets the saved color from what I set it as
+    const foreground = localStorage.getItem("foreground-color"); //gets the saved color from what I set it as
 
     if (savedName){ //if there IS a saved name
         document.getElementById("greeting").innerText = "Hello " + savedName; //set the greeting with the name
@@ -23,15 +24,22 @@ window.onload = function () { //Initializes when the window of the website loads
         document.body.style.backgroundColor = savedColor; //set the body background to the saved color they set
         document.getElementById("background-color").value = savedColor; //set the color to the saved color
     }
+    if (foreground){ //if there IS a foreground color
+        document.body.style.color = foreground; //set the body background to the saved color they set
+        document.getElementById("foreground-color").value = foreground; //set the color to the saved color
+    }   
 }
 
 document.getElementById("submit").onclick = function() { // When the submit button is clicked
     const name = document.getElementById("name").value; //get the name from the form
     const color = document.getElementById("background-color").value; //get the background color from the form
+    const fcolor = document.getElementById("foreground-color").value; //get the foreground color from the form
 
     localStorage.setItem("name", name); //set the name in local storage so it is remembered
     localStorage.setItem("background-color", color); //set the background color in local storage so it is remembered
+    localStorage.setItem("foreground-color", fcolor); //set the foreground color in local storage so it is remembered
 
     document.getElementById("greeting").innerText = "Hello " + name; //set the greeting with the name
     document.body.style.backgroundColor = color; //set the body background to the color
+    document.body.style.color = fcolor; //set the body foreground to the color
 }
